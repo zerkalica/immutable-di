b7bf03e3b7eb1655d244f9bc01309f1d3ec3e6d8
Stefan Zerkalica <zerkalica@gmail.com>
2015-03-23 16:41:36 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-import DiBuilder from './immutable-di-builder'
+import ContainerCreator from './container-creator'
+import Container from './container'
+
 import NativeState from './state-adapters/native-adapter'
 
 import ReactRenderer from './flux/renderers/react-renderer'
 import WrapActionMethods from './flux/wrap-action-methods'
+
 import Dispatcher from './flux/dispatcher'
 import Renderer from './flux/renderer'
 import Context from './flux/context'
 
 export default {
-    DiBuilder,
+    ContainerCreator,
+    Container,
     NativeState,
     ReactRenderer,
     Dispatcher,
