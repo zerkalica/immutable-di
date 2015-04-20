ae5fb8dd7e3402601a71dc62c69242871fba1992
Stefan Zerkalica <zerkalica@gmail.com>
2015-04-21 00:56:59 +0300

    rebuilded dist

--- dist/flux/connectors/react-connector.js
+++ dist/flux/connectors/react-connector.js
@@ -1,67 +1,82 @@
-"use strict";
+'use strict';
 
-var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } };
+var _interopRequireWildcard = function (obj) { return obj && obj.__esModule ? obj : { 'default': obj }; };
 
-var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
+var _classCallCheck = function (instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError('Cannot call a class as a function'); } };
 
-var _get = function get(object, property, receiver) { var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ("value" in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
+var _createClass = (function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ('value' in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; })();
 
-var _inherits = function (subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) subClass.__proto__ = superClass; };
+var _get = function get(object, property, receiver) { var desc = Object.getOwnPropertyDescriptor(object, property); if (desc === undefined) { var parent = Object.getPrototypeOf(object); if (parent === null) { return undefined; } else { return get(parent, property, receiver); } } else if ('value' in desc) { return desc.value; } else { var getter = desc.get; if (getter === undefined) { return undefined; } return getter.call(receiver); } };
 
-Object.defineProperty(exports, "__esModule", {
+var _inherits = function (subClass, superClass) { if (typeof superClass !== 'function' && superClass !== null) { throw new TypeError('Super expression must either be null or a function, not ' + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) subClass.__proto__ = superClass; };
+
+Object.defineProperty(exports, '__esModule', {
     value: true
 });
-exports["default"] = getReactConnector;
+exports['default'] = getReactConnector;
 
-function getReactConnector(React) {
+var _Dispatcher = require('../dispatcher');
+
+var _Dispatcher2 = _interopRequireWildcard(_Dispatcher);
+
+function getReactConnector(React, childContextTypes) {
+    var p = React.PropTypes;
+
     var ComponentWrapper = (function (_React$Component) {
-        function ComponentWrapper(_ref) {
-            var state = _ref.state;
-            var dispatcher = _ref.dispatcher;
-            var getter = _ref.getter;
-            var component = _ref.component;
-            var context = _ref.context;
-
+        function ComponentWrapper(props, context) {
             _classCallCheck(this, ComponentWrapper);
 
-            _get(Object.getPrototypeOf(ComponentWrapper.prototype), "constructor", this).call(this, state);
-            this.state = state;
-            this._dispatcher = dispatcher;
-            this._getter = getter;
-            this._component = component;
-            this._context = context;
+            _get(Object.getPrototypeOf(ComponentWrapper.prototype), 'constructor', this).call(this, props, context);
+            this.__listener = null;
+            this.state = props.state;
         }
 
         _inherits(ComponentWrapper, _React$Component);
 
         _createClass(ComponentWrapper, [{
-            key: "getChildContext",
+            key: '__listener',
+            value: undefined,
+            enumerable: true
+        }, {
+            key: 'getChildContext',
             value: function getChildContext() {
-                return this._context;
+                return {
+                    actions: this.props.actions
+                };
             }
         }, {
-            key: "componentDidMount",
+            key: 'componentDidMount',
             value: function componentDidMount() {
                 var _this = this;
 
-                this._listener = this._dispatcher.mount(this._getter, function (state) {
+                this.__listener = this.props.dispatcher.mount(this.props.getter, function (state) {
                     return _this.setState(state);
                 });
             }
         }, {
-            key: "componentWillUnmount",
+            key: 'componentWillUnmount',
             value: function componentWillUnmount() {
-                this._dispatcher.unmount(this._listener);
+                this.props.dispatcher.unmount(this.__listener);
             }
         }, {
-            key: "render",
+            key: 'render',
             value: function render() {
-                return React.createElement(this._component, this.state);
+                return React.createElement(this.props.component, this.props.state);
             }
         }], [{
-            key: "childContextTypes",
+            key: 'childContextTypes',
             value: {
-                actions: React.PropTypes.object
+                actions: p.object.isRequired
+            },
+            enumerable: true
+        }, {
+            key: 'propTypes',
+            value: {
+                dispatcher: p.instanceOf(_Dispatcher2['default']).isRequired,
+                state: p.object.isRequired,
+                getter: p.func.isRequired,
+                component: p.func.isRequired,
+                actions: p.object.isRequired
             },
             enumerable: true
         }]);
@@ -72,4 +87,4 @@ function getReactConnector(React) {
     return ComponentWrapper;
 }
 
-module.exports = exports["default"];
+module.exports = exports['default'];
