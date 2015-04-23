1fff5c821b4349b765723ce86826766db8b4dd71
Yuferev Sergey <s.yuferev@qiwi.ru>
2015-04-23 11:27:13 +0300

    unused test removed

--- src/__tests__/utils-test.js
+++ src/__tests__/utils-test.js
@@ -1,40 +1,7 @@
-import {isPromise, getDebugPath, classToFactory, getFunctionName} from '../utils'
+import {getDebugPath, getFunctionName} from '../utils'
 import {describe, it, expect} from '../test-helper'
 
 describe('utils', () => {
-    describe('classToFactory', () => {
-        class TestC {
-        }
-
-        class TestC1 {
-            constructor(a, b) {
-                if (a !== 1 || b !== 2) {
-                    throw new Error('Invalid arguments');
-                }
-                this.c = a + b;
-            }
-        }
-
-        it('should create function from class', () => {
-            classToFactory(TestC).should.to.be.function;
-        })
-
-        it('should create factory function, which returns instance of class', () => {
-            classToFactory(TestC)().should.to.be.instanceOf(TestC);
-        })
-
-        it('should call constructor of original class', () => {
-            let factory = classToFactory(TestC1);
-            (() => factory()).should.to.throw('Invalid arguments');
-        })
-
-        it('should pass arguments to constructor', () => {
-            let factory = classToFactory(TestC1);
-            (() => factory(1, 2)).should.not.to.throw();
-            expect(factory(1, 2).c).equal(3)
-        })
-    })
-
     describe('getDebugPath', () => {
         it('should return string', () => {
             getDebugPath().should.to.be.equal('unk')
