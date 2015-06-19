ebc99448b18331c497d3e1d043df2ca69e5392e6
Stefan Zerkalica <zerkalica@gmail.com>
2015-06-19 17:48:22 +0300

    updated deps

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-npm i --save debug babel-runtime
+npm i --save debug babel-runtime react
 
-npm i --save-dev babel-core babel babel-plugin-closure-elimination \
+npm i --save-dev babel-core babel \
     eslint babel-eslint \
     mocha sinon babel-plugin-espower power-assert proxyquire \
     webpack webpack-dev-server babel-loader html-webpack-plugin react
