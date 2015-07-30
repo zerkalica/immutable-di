5327a37785f3142f02ab7e691a8cbedc7aa8679d
Stefan Zerkalica <zerkalica@gmail.com>
2015-07-30 16:07:57 +0300

    fix

--- test/tests.js
+++ test/tests.js
@@ -1,7 +1,2 @@
 import glob from 'glob'
-import 'source-map-support/register'
-
-const mask = 'src/**/__tests__/*.js'
-const srcMask = __dirname + '/../' + mask
-//console.log(srcMask)
-glob.sync(srcMask).forEach(file => require(file))
+glob.sync(__dirname + '/../src/**/__tests__/*.js').forEach(file => require(file))
