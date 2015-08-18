797a66e4f0c31f4fabdd992f1d8c4204c539781f
Stefan Zerkalica <zerkalica@gmail.com>
2015-08-19 00:46:34 +0300

    fix

--- define.js
+++ define.js
@@ -1,155 +1,35 @@
 'use strict';
 
-var _Object$keys = require('babel-runtime/core-js/object/keys')['default'];
-
 var _interopRequireDefault = require('babel-runtime/helpers/interop-require-default')['default'];
 
 Object.defineProperty(exports, '__esModule', {
     value: true
 });
-exports.Class = Class;
-exports.Factory = Factory;
-exports.Facet = Facet;
 exports.Getter = Getter;
 exports.Path = Path;
 exports.Assign = Assign;
 exports.Setter = Setter;
 exports.Def = Def;
+exports.Class = Class;
+exports.Factory = Factory;
+exports.Facet = Facet;
 
-var _utilsGetFunctionName = require('./utils/get-function-name');
+var _cursorsAbstract = require('./cursors/abstract');
 
-var _utilsGetFunctionName2 = _interopRequireDefault(_utilsGetFunctionName);
+var _cursorsAbstract2 = _interopRequireDefault(_cursorsAbstract);
 
-var __Container = '__DICONTAINER__';
+var _utilsDep = require('./utils/Dep');
 
-exports.__Container = __Container;
-function normalizeDeps(d) {
-    var resultDeps = [];
-    var deps = d || [];
-    var isArray = Array.isArray(deps);
-    var names = isArray ? [] : _Object$keys(deps);
-    var len = isArray ? deps.length : names.length;
-
-    for (var i = 0; i < len; i++) {
-        var _name = names.length ? names[i] : undefined;
-        var dep = deps[_name || i];
-        resultDeps.push({
-            name: _name,
-            definition: Array.isArray(dep) ? Path(dep) : dep
-        });
-    }
-
-    return resultDeps;
-}
-
-function updateIdsMap(acc, normalizedDeps) {
-    for (var i = 0, j = normalizedDeps.length; i < j; i++) {
-        var dep = normalizedDeps[i];
-        if (dep.definition === __Container) {
-            break;
-        }
-        var _dep$definition$__di = dep.definition.__di;
-        var _path = _dep$definition$__di.path;
-        var isSetter = _dep$definition$__di.isSetter;
-        var deps = _dep$definition$__di.deps;
-
-        if (isSetter) {
-            acc.isAction = true;
-        }
-
-        if (_path && _path.length) {
-            if (acc.id) {
-                var parts = [];
-                for (var ii = 0, jj = _path.length; ii < jj; ii++) {
-                    parts.push(_path[ii]);
-                    var key = parts.toString();
-                    var ids = acc.map[key];
-                    if (!ids) {
-                        ids = [];
-                        acc.map[key] = ids;
-                    }
-                    if (ids.indexOf(acc.id) === -1) {
-                        ids.push(acc.id);
-                    }
-                }
-            }
-        } else {
-            updateIdsMap(acc, deps);
-        }
-    }
-}
-
-var lastId = 1;
-var __pathToIdsMap = {};
-
-exports.__pathToIdsMap = __pathToIdsMap;
-function Dep(_ref) {
-    var id = _ref.id;
-    var displayName = _ref.displayName;
-    var deps = _ref.deps;
-    var isClass = _ref.isClass;
-    var isCachedTemporary = _ref.isCachedTemporary;
-    var isSetter = _ref.isSetter;
-    var path = _ref.path;
-
-    return function dep(Service) {
-        id = id || (Service.__di ? Service.__di.id : lastId++);
-        var dn = displayName || Service.displayName || (0, _utilsGetFunctionName2['default'])(Service) || id;
-        var newDeps = normalizeDeps(deps);
-        var acc = {
-            map: __pathToIdsMap,
-            isAction: false,
-            id: isCachedTemporary ? null : id
-        };
-        updateIdsMap(acc, newDeps);
-
-        Service.__di = {
-            deps: newDeps,
-            displayName: dn,
-            id: id,
-            isAction: acc.isAction,
-            isSetter: !!isSetter,
-            isCachedTemporary: !!isCachedTemporary,
-            isClass: !!isClass,
-            isOptions: !Array.isArray(deps),
-            path: path
-        };
-
-        return Service;
-    };
-}
-
-function Class(deps, displayName) {
-    return Dep({
-        deps: deps,
-        displayName: displayName,
-        isClass: true
-    });
-}
-
-function Factory(deps, displayName) {
-    return Dep({
-        deps: deps,
-        displayName: displayName
-    });
-}
-
-function Facet(deps, displayName) {
-    return Dep({
-        deps: deps,
-        displayName: displayName,
-        isCachedTemporary: true
-    });
-}
+var _utilsDep2 = _interopRequireDefault(_utilsDep);
 
 function Getter(path) {
     var key = path.join('.');
     var id = 'get#' + key;
-    function getter(container) {
-        return container.select(path).get;
+    function getter(cursor) {
+        return cursor.select(path).get;
     }
-    return Dep({
-        deps: [__Container],
+    return (0, _utilsDep2['default'])({
+        deps: [_cursorsAbstract2['default']],
         displayName: id,
         id: id
     })(getter);
@@ -162,7 +42,7 @@ function Path(path) {
         return get();
     }
 
-    return Dep({
+    return (0, _utilsDep2['default'])({
         deps: [Getter(path)],
         displayName: id,
         id: id,
@@ -174,11 +54,12 @@ function Path(path) {
 function Assign(path) {
     var key = path.join('.');
     var id = 'assign#' + key;
-    function assigner(container) {
-        return container.select(path).assign;
+    function assigner(cursor) {
+        return cursor.select(path).assign;
     }
-    return Dep({
-        deps: [__Container],
+
+    return (0, _utilsDep2['default'])({
+        deps: [_cursorsAbstract2['default']],
         displayName: id,
         id: id,
         isSetter: true
@@ -188,12 +69,12 @@ function Assign(path) {
 function Setter(path) {
     var key = path.join('.');
     var id = 'setter#' + key;
-    function setter(container) {
-        return container.select(path).set;
+    function setter(cursor) {
+        return cursor.select(path).set;
     }
 
-    return Dep({
-        deps: [__Container],
+    return (0, _utilsDep2['default'])({
+        deps: [_cursorsAbstract2['default']],
         displayName: id,
         id: id,
         isSetter: true
@@ -206,9 +87,35 @@ function Def(data) {
         return data;
     }
 
-    return Dep({
+    return (0, _utilsDep2['default'])({
         displayName: id,
         id: id
     })(def);
+}
+
+function Class(deps, displayName) {
+    return (0, _utilsDep2['default'])({
+        deps: deps,
+        displayName: displayName,
+        isClass: true,
+        pathMapper: Path
+    });
+}
+
+function Factory(deps, displayName) {
+    return (0, _utilsDep2['default'])({
+        deps: deps,
+        displayName: displayName,
+        pathMapper: Path
+    });
+}
+
+function Facet(deps, displayName) {
+    return (0, _utilsDep2['default'])({
+        deps: deps,
+        displayName: displayName,
+        isCachedTemporary: true,
+        pathMapper: Path
+    });
 }
 //# sourceMappingURL=define.js.map
