5ec161885b34bf2ebf8243a1a72be553b73bddac
Stefan Zerkalica <zerkalica@gmail.com>
2015-08-19 13:00:12 +0300

    fix

--- define.js
+++ define.js
@@ -11,8 +11,8 @@ exports.Assign = Assign;
 exports.Setter = Setter;
 exports.Def = Def;
 exports.Class = Class;
-exports.Factory = Factory;
 exports.Facet = Facet;
+exports.Factory = Factory;
 
 var _cursorsAbstract = require('./cursors/abstract');
 
@@ -22,30 +22,50 @@ var _utilsDep = require('./utils/Dep');
 
 var _utilsDep2 = _interopRequireDefault(_utilsDep);
 
+var _historyMonitorFactory = require('./history/MonitorFactory');
+
+var _historyMonitorFactory2 = _interopRequireDefault(_historyMonitorFactory);
+
+var ids = {};
+
+function convertId(dn) {
+    if (!ids[dn]) {
+        ids[dn] = (0, _utilsDep.getId)();
+    }
+
+    return ids[dn];
+}
+
+var settings = {
+    debug: false
+};
+
+exports.settings = settings;
+
 function Getter(path) {
     var key = path.join('.');
-    var id = 'get#' + key;
+    var displayName = 'get#' + key;
     function getter(cursor) {
         return cursor.select(path).get;
     }
     return (0, _utilsDep2['default'])({
         deps: [_cursorsAbstract2['default']],
-        displayName: id,
-        id: id
+        displayName: displayName,
+        id: convertId(displayName)
     })(getter);
 }
 
 function Path(path) {
     var key = path.join('.');
-    var id = 'path#' + key;
+    var displayName = 'path#' + key;
     function getData(get) {
         return get();
     }
 
     return (0, _utilsDep2['default'])({
         deps: [Getter(path)],
-        displayName: id,
-        id: id,
+        displayName: displayName,
+        id: convertId(displayName),
         isCachedTemporary: true,
         path: path
     })(getData);
@@ -53,43 +73,43 @@ function Path(path) {
 
 function Assign(path) {
     var key = path.join('.');
-    var id = 'assign#' + key;
+    var displayName = 'assign#' + key;
     function assigner(cursor) {
         return cursor.select(path).assign;
     }
 
     return (0, _utilsDep2['default'])({
         deps: [_cursorsAbstract2['default']],
-        displayName: id,
-        id: id,
+        displayName: displayName,
+        id: convertId(displayName),
         isSetter: true
     })(assigner);
 }
 
 function Setter(path) {
     var key = path.join('.');
-    var id = 'setter#' + key;
+    var displayName = 'setter#' + key;
     function setter(cursor) {
         return cursor.select(path).set;
     }
 
     return (0, _utilsDep2['default'])({
         deps: [_cursorsAbstract2['default']],
-        displayName: id,
-        id: id,
+        displayName: displayName,
+        id: convertId(displayName),
         isSetter: true
     })(setter);
 }
 
 function Def(data) {
-    var id = 'def#' + JSON.stringify(data);
+    var displayName = 'def#' + JSON.stringify(data);
     function def() {
         return data;
     }
 
     return (0, _utilsDep2['default'])({
-        displayName: id,
-        id: id
+        displayName: displayName,
+        id: convertId(displayName)
     })(def);
 }
 
@@ -102,14 +122,6 @@ function Class(deps, displayName) {
     });
 }
 
-function Factory(deps, displayName) {
-    return (0, _utilsDep2['default'])({
-        deps: deps,
-        displayName: displayName,
-        pathMapper: Path
-    });
-}
-
 function Facet(deps, displayName) {
     return (0, _utilsDep2['default'])({
         deps: deps,
@@ -117,5 +129,15 @@ function Facet(deps, displayName) {
         isCachedTemporary: true,
         pathMapper: Path
     });
+}
+
+function Factory(deps, displayName) {
+    var origDep = (0, _utilsDep2['default'])({
+        deps: deps,
+        displayName: displayName,
+        pathMapper: Path
+    });
+
+    return settings.debug ? (0, _historyMonitorFactory2['default'])(origDep) : origDep;
 }
 //# sourceMappingURL=define.js.map
