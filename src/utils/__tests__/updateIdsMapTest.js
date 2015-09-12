4ca704f0a7fdcec9c448f3503eb93766ff8f3249
Stefan Zerkalica <zerkalica@gmail.com>
2015-09-13 00:13:40 +0300

    tests

--- src/utils/__tests__/updateIdsMapTest.js
+++ src/utils/__tests__/updateIdsMapTest.js
@@ -8,7 +8,7 @@ function cmpArr(a1, a2) {
 }
 
 describe('updateIdsMap', () => {
-    it('(B->a)->(A->a): to a: [A, B]', () => {
+    it('B->a, A->a, B->A: to a: [A, B]', () => {
         const pathToIdsMap = {}
         const idToPathsMap = {}
         const DepA = Factory([['a']])(v => v)
@@ -20,7 +20,40 @@ describe('updateIdsMap', () => {
         assert(cmpArr(pathToIdsMap.a, ['A', 'B']))
     })
 
-    it('B->(A->a), C->(A->a) to a: [A, B, C]', () => {
+    it('A->C, A->B, B->C, C->a, B->b to a: [A, B, C], b: [A, B]', () => {
+        const pathToIdsMap = {}
+        const idToPathsMap = {}
+        const DepC = Factory([['a']])(v => v)
+        const DepB = Factory([DepC, ['b']])(v => v)
+        const DepA = Factory([DepC, DepB])(v => v)
+        DepA.__di.id = 'A'
+        DepB.__di.id = 'B'
+        DepC.__di.id = 'C'
+
+        updateIdsMap(DepA, pathToIdsMap, idToPathsMap)
+        assert(cmpArr(pathToIdsMap.a, ['A', 'B', 'C']))
+        assert(cmpArr(pathToIdsMap.b, ['A', 'B']))
+    })
+
+    it('A->C, A->B, B->C, C->a, B->b, then add D->B to a: [A, B, C, D], b: [A, B, D]', () => {
+        const pathToIdsMap = {}
+        const idToPathsMap = {}
+        const DepC = Factory([['a']])(v => v)
+        const DepB = Factory([DepC, ['b']])(v => v)
+        const DepA = Factory([DepC, DepB])(v => v)
+        const DepD = Factory([DepB])(v => v)
+        DepA.__di.id = 'A'
+        DepB.__di.id = 'B'
+        DepC.__di.id = 'C'
+        DepD.__di.id = 'D'
+
+        updateIdsMap(DepA, pathToIdsMap, idToPathsMap)
+        updateIdsMap(DepD, pathToIdsMap, idToPathsMap)
+        assert(cmpArr(pathToIdsMap.a, ['A', 'B', 'C', 'D']))
+        assert(cmpArr(pathToIdsMap.b, ['A', 'B', 'D']))
+    })
+
+    it('B->A, C->A, A->a to a: [A, B, C]', () => {
         const pathToIdsMap = {}
         const idToPathsMap = {}
         const DepA = Factory([['a']])(v => v)
@@ -33,5 +66,46 @@ describe('updateIdsMap', () => {
         updateIdsMap(DepB, pathToIdsMap, idToPathsMap)
         updateIdsMap(DepC, pathToIdsMap, idToPathsMap)
         assert(cmpArr(pathToIdsMap.a, ['A', 'B', 'C']))
+    })
+
+    it('B->A, C->B, A->a to a: [A, B, C]', () => {
+        const pathToIdsMap = {}
+        const idToPathsMap = {}
+        const DepA = Factory([['a']])(v => v)
+        const DepB = Factory([DepA])(v => v)
+        const DepC = Factory([DepB])(v => v)
+        DepA.__di.id = 'A'
+        DepB.__di.id = 'B'
+        DepC.__di.id = 'C'
+
+        updateIdsMap(DepB, pathToIdsMap, idToPathsMap)
+        updateIdsMap(DepC, pathToIdsMap, idToPathsMap)
+        assert(cmpArr(pathToIdsMap.a, ['A', 'B', 'C']))
+    })
+
+    it('B->A, A->a.b, B->a to a: [A, B], a.b: [A, B]', () => {
+        const pathToIdsMap = {}
+        const idToPathsMap = {}
+        const DepA = Factory([['a', 'b']])(v => v)
+        const DepB = Factory([DepA, ['a']])(v => v)
+        DepA.__di.id = 'A'
+        DepB.__di.id = 'B'
+
+        updateIdsMap(DepB, pathToIdsMap, idToPathsMap)
+        assert(cmpArr(pathToIdsMap.a, ['A', 'B']))
+        assert(cmpArr(pathToIdsMap['a.b'], ['A', 'B']))
+    })
+
+    it('B->A, A->a, B->b to a: [A, B], b: [B]', () => {
+        const pathToIdsMap = {}
+        const idToPathsMap = {}
+        const DepA = Factory([['a']])(v => v)
+        const DepB = Factory([DepA, ['b']])(v => v)
+        DepA.__di.id = 'A'
+        DepB.__di.id = 'B'
+
+        updateIdsMap(DepB, pathToIdsMap, idToPathsMap)
+        assert(cmpArr(pathToIdsMap.a, ['A', 'B']))
+        assert(cmpArr(pathToIdsMap.b, ['B']))
     })
 })
